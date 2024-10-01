65bd2f4d580de388e13a3fcd8e8a4875fa1c9e8d
Ali Moji <alimojarrad2003@gmail.com>
2024-10-01 11:56:10 +0300

    solved some few errors

--- uploader.py
+++ uploader.py
@@ -14,27 +14,26 @@ file_id = {}
 # use this in order to save the stats of a user
 user_stat = {}
 # if you have a sponsor channel save it here
-sponsor_channel = -10012345678
+sponsor_channel = -1001234567
 # register admin user here in order to work properly
-admin = 123345678
+admin = 123456789
 @bot.message_handler(commands=["start"])
 def greeting(msg):
     try:
-        if len(msg.text) > 1:
+       bot.reply_to(msg, "hello")
+       if len(msg.text) > 1:
           user_stat[msg.from_user.id] = msg.text.split()[1]
-          name = msg.text.split()[1]
-          member = bot.get_chat_member(sponsor_channel, msg.from_usrer.id)
-          if member in ["member", "creator", "administrator"]:
-            bot.send_video(msg.from_user.id, file_id.get(name))
+          the_id = msg.text.split()[1]
+          member = bot.get_chat_member(sponsor_channel, msg.from_user.id)
+          if member.status in ["member", "creator", "administrator"]:
+            bot.send_video(msg.from_user.id, file_id.get(the_id))
           else:
-             markup = quick_markup({
+            markup = quick_markup({
                 "Check" :{"callback_data": "check"}
              }, row_width=1)
-             bot.send_message(msg.from_user.id, "You have to join our sponsor channel", reply_markup=markup)
-        else:
-          bot.reply_to(msg, "What can I do for you?")
+            bot.send_message(msg.from_user.id, "join the sponsor channel", reply_markup=markup)
     except Exception as e:
-       bot.reply_to(msg, "sorry there has been an error")
+       bot.reply_to(msg, "try again later")
        bot.send_message(admin, f"error as {e}")
 
 @bot.message_handler(content_types=["video"])
@@ -53,16 +52,17 @@ def getting_id(msg):
    except Exception as e:
       bot.send_message(admin, f"Error in creating your video link as {e}")
    
-@bot.callback_query_handler(func=lambda call:call.id == "check")
+@bot.callback_query_handler(func=lambda call:call.data == "check")
 def checking_user(call):
    try:
       user = call.from_user.id
-      member = bot.get_chat_member(user, sponsor_channel)
-      if member in ["member", "creator", "administrator"]:
+      member = bot.get_chat_member(sponsor_channel, user)
+      if member.status in ["member", "creator", "administrator"]:
             file_id_1 = user_stat.get(user)
             bot.send_video(call.from_user.id, file_id.get(file_id_1))
+            bot.delete_message(call.from_user.id, call.message.id)
       else:
-         bot.answer_callback_query("You didn't join yet", show_alert=True)
+         bot.answer_callback_query(call.id, "You didn't join yet", show_alert=True)
    except Exception as e:
       bot.send_message(call.from_user.id, "error ocurred. Try again later.")
       bot.send_message(admin, f"Error for {call.from_user.id} to get the video as {e}")
